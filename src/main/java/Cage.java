2be090a9fe853d603a0feebcd6687759a52c7722
WholeHorse <whitecrazygoose@mail.ru>
2023-11-21 19:43:38 +0300

    Lab4 done

--- src/main/java/Cage.java
+++ src/main/java/Cage.java
@@ -1,14 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Cage {
-    private Animal animal;
+    private List<Animal> animals;
+
+    public Cage() {
+        animals = new ArrayList<>();
+    }
 
     public void add(Animal animal) throws Exception {
-        if (this.animal != null) {
-            throw new Exception("Клетка уже занята!");
+        if (animals.size() >= 4) {
+            throw new Exception("Клетка уже заполнена!");
         }
-        this.animal = animal;
+        for (Animal existingAnimal : animals) {
+            if (!isCompatible(animal, existingAnimal)) {
+                throw new Exception("Животные несовместимы!");
+            }
+        }
+        animals.add(animal);
     }
 
-    public Animal getAnimal() {
-        return animal;
+    private boolean isCompatible(Animal animal1, Animal animal2) {
+        if (animal1.getAnimalType().equals("Хищник") && animal2.getAnimalType().equals("Хищник")
+                && !animal1.getAnimalClass().equals(animal2.getAnimalClass())) {
+            return false;
+        }
+        if (!animal1.getAnimalType().equals(animal2.getAnimalType())) {
+            return false;
+        }
+        return true;
     }
+
+    public List<Animal> getAnimals() {
+        return animals;
+    }
+
+
 }
--- src/main/java/Zoo.java
+++ src/main/java/Zoo.java
@@ -1,25 +1,136 @@
-class Zoo {
-    private final Cage[] cages;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collections;
+
+class Zoo implements Iterable<Animal> {
+    private List<Cage> cages;
 
     public Zoo(int cageCount) {
-        cages = new Cage[cageCount];
+        cages = new ArrayList<>();
         for (int i = 0; i < cageCount; i++) {
-            cages[i] = new Cage();
+            cages.add(new Cage());
         }
     }
 
-    public void addAnimal(Animal animal, int cageIndex) throws Exception {
-        cages[cageIndex].add(animal);
+    public Set<String> getUniqueAnimalClasses() {
+        Set<String> uniqueAnimalClasses = new HashSet<>();
+        for (Cage cage : cages) {
+            for (Animal animal : cage.getAnimals()) {
+                uniqueAnimalClasses.add(animal.getAnimalClass());
+            }
+        }
+        return uniqueAnimalClasses;
     }
 
-    public void walkThroughZoo() {
-        for (int i = 0; i < cages.length; i++) {
-            Animal animal = cages[i].getAnimal();
-            if (animal != null) {
-                System.out.println("Клетка " + (i+1) + ": " + animal.getAnimalSound());
-            } else {
-                System.out.println("Клетка " + (i+1) + " пуста.");
+    public Set<String> getUniqueAnimalTypes() {
+        Set<String> uniqueAnimalTypes = new HashSet<>();
+        for (Cage cage : cages) {
+            for (Animal animal : cage.getAnimals()) {
+                uniqueAnimalTypes.add(animal.getAnimalType());
             }
+        }
+        return uniqueAnimalTypes;
+    }
+
+    public Map<String, Integer> countAnimalClasses() {
+        Map<String, Integer> animalCounts = new HashMap<>();
+        for (Cage cage : cages) {
+            for (Animal animal : cage.getAnimals()) {
+                String animalClass = animal.getAnimalClass();
+                if (animalCounts.containsKey(animalClass)) {
+                    animalCounts.put(animalClass, animalCounts.get(animalClass) + 1);
+                } else {
+                    animalCounts.put(animalClass, 1);
+                }
+            }
+        }
+        return animalCounts;
+    }
+
+    public Map<String, Integer> countAnimalTypes() {
+        Map<String, Integer> animalCounts = new HashMap<>();
+        for (Cage cage : cages) {
+            for (Animal animal : cage.getAnimals()) {
+                String animalType = animal.getAnimalType();
+                if (animalCounts.containsKey(animalType)) {
+                    animalCounts.put(animalType, animalCounts.get(animalType) + 1);
+                } else {
+                    animalCounts.put(animalType, 1);
+                }
+            }
+        }
+        return animalCounts;
+    }
+
+    public List<String> getAnimalsByCount() {
+        Map<String, Integer> animalCounts = countAnimalClasses();
+        List<String> animalsByCount = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : animalCounts.entrySet()) {
+            animalsByCount.add(entry.getKey() + ": " + entry.getValue());
+        }
+        Collections.sort(animalsByCount, Collections.reverseOrder());
+        return animalsByCount;
+    }
+
+    public void addAnimal(Animal animal, int cageIndex) throws Exception {
+        cages.get(cageIndex).add(animal);
+    }
+
+    public void addCages(int additionalCageCount) {
+        for (int i = 0; i < additionalCageCount; i++) {
+            cages.add(new Cage());
+        }
+    }
+
+    @Override
+    public Iterator<Animal> iterator() {
+        return new Iterator<Animal>() {
+            private int cageIndex = 0;
+            private int animalIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                while (cageIndex < cages.size() && (cages.get(cageIndex).getAnimals() == null
+                        || animalIndex >= cages.get(cageIndex).getAnimals().size())) {
+                    cageIndex++;
+                    animalIndex = 0;
+                }
+                return cageIndex < cages.size();
+            }
+
+            @Override
+            public Animal next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return cages.get(cageIndex).getAnimals().get(animalIndex++);
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException("remove");
+            }
+        };
+    }
+
+    public void walk() {
+        int i = 1;
+        for (Cage cage : cages) {
+            List<Animal> animals = cage.getAnimals();
+            if (animals.isEmpty()) {
+                System.out.println("Клетка " + i + " пуста.");
+            } else {
+                for (Animal animal : animals) {
+                    System.out.println("Клетка " + i + ": " + animal.getAnimalSound());
+                }
+            }
+            i++;
         }
     }
 }
